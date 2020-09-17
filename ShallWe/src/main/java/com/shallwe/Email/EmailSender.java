ba5addc5b396036ffebd31960bc6d475821a6584
captic94 <seilu94@naver.com>
2020-09-17 10:36:47 +0900

    줄 정리

--- ShallWe/src/main/java/com/shallwe/Email/EmailSender.java
+++ ShallWe/src/main/java/com/shallwe/Email/EmailSender.java
@@ -10,37 +10,32 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
 public class EmailSender {
-	
-
-	 @Autowired
+	@Autowired
     protected JavaMailSender  mailSender;
 	 
     public void SendEmail(Email email) throws Exception {
-     MimeMessage msg = mailSender.createMimeMessage();
+    	
+    	MimeMessage msg = mailSender.createMimeMessage();
        
-        try {
-        		
-        	
-			MimeMessageHelper messageHelper 
-			= new MimeMessageHelper(msg, true, "UTF-8");
+	    try {
+			MimeMessageHelper messageHelper = new MimeMessageHelper(msg, true, "UTF-8");
 			
-            msg.setSubject(email.getSubject());
-            msg.setText(email.getContent());
-            msg.setRecipients(MimeMessage.RecipientType.TO , InternetAddress.parse(email.getReceiver()));
-
-        }catch(MessagingException e) {
-            System.out.println("MessagingException");
-            e.printStackTrace();
-        }
-
-        try {
-            mailSender.send(msg);
-        }catch(MailException e) {
-            System.out.println("MailException발생");
-            e.printStackTrace();
-
-        }
-
+	        msg.setSubject(email.getSubject());
+	        msg.setText(email.getContent());
+	        msg.setRecipients(MimeMessage.RecipientType.TO , InternetAddress.parse(email.getReceiver()));
+	
+	    }catch(MessagingException e) {
+	        System.out.println("MessagingException");
+	        e.printStackTrace();
+	    }
+	
+	    try {
+	        mailSender.send(msg);
+	    }catch(MailException e) {
+	        System.out.println("MailException발생");
+	        e.printStackTrace();
+	
+	    }
     }
 
 }
