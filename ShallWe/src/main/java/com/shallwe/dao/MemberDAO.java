98d2f051e8e684860910d0ffe7c24777bdf58af6
kimsangha617 <66237694+kimsangha617@users.noreply.github.com>
2020-09-17 18:05:26 +0900

    info 보기 pw 변경

--- ShallWe/src/main/java/com/shallwe/dao/MemberDAO.java
+++ ShallWe/src/main/java/com/shallwe/dao/MemberDAO.java
@@ -1,46 +1,95 @@
 package com.shallwe.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+
+import com.shallwe.exception.AddException;
 import com.shallwe.exception.FindException;
 import com.shallwe.exception.ModifyException;
 import com.shallwe.model.MemberInfoBean;
 import com.shallwe.vo.LectureCategory;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-@Repository(value = "memberDAO")
+@Repository
+@Qualifier(value = "memberDAO")
 public class MemberDAO {
 	@Autowired
 	SqlSessionFactory sqlSessionFactory;
 	// 회원가입, 로그인, 아이디 찾기, 비밀번호 찾기(비회원) : 경찬	-> 이거 메소드 1개도 아니고 생성, 조회, 수정, 삭제 아닌 애들은 다 서비스에 있어야 합니다 참고해주세요
 	
+
+	// 회원가입 : 상하
+//	public void joinMember(Map<String, Object>joinmem, MemberInfoBean mib) throws AddException{
+//		SqlSession session=null;
+//		session = sqlSessionFactory.openSession();
+//		mib = session.insert("MemberMapper.joinMember", parameter)
+		
+//	}
 	// 내 정보 보기(수강생) : 상하
-	MemberInfoBean selectById(String member_id) throws FindException{
-		return null;
+	public MemberInfoBean selectById(String memberId) throws FindException
+	{
+		SqlSession session = null;
+		try {
+		session = sqlSessionFactory.openSession();
+		MemberInfoBean m = session.selectOne("MemberMapper.selectById", memberId);
+		if(m==null) {
+			throw new FindException("조회된 회원이 없습니다");
+		}
+		return m;
+		}catch(DataAccessException e) {
+			e.printStackTrace();
+			throw new FindException(e.getMessage());
+		}finally {
+			session.close();
+		}
 	}
 	
 	// 패스워드 수정 : 상하
-	void updatePassword(String member_id, String member_password) throws ModifyException{
-		
+	public void updatePassword(String memberId, String memberPwd) throws ModifyException{
+		SqlSession session = null;
+		Map<String,String>memIdPwd = new HashMap<String, String>();
+		memIdPwd.put("memberId", memberId);
+		memIdPwd.put("memberPwd", memberPwd);
+			
+		int i = 0;
+		try {
+			session = sqlSessionFactory.openSession();
+			i = session.update("MemberMapper.updatePassword",memIdPwd);
+			if(i==0) {
+			 throw new ModifyException("비밀번호 수정에 실패하였습니다");
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			throw new ModifyException(e.getMessage());
+		}finally{
+			session.close();
+		}
+	
 	}
 	
 	//이메일수정 : 상하
-	void updateEmail(String member_id, String member_email)throws ModifyException{
+	void updateEmail(String memberId, String memberEmail)throws ModifyException{
 		
 	}
 	
 	//핸드폰번호수정 :상하
-	void updatePhone(String member_id, String member_phone)throws ModifyException{
+	void updatePhone(String memberId, String memberPhone)throws ModifyException{
 		
 	}
 	
 	//선호카테고리 수정: 상하
-	void updateFavorites(String member_id, List<LectureCategory> favorite_list)throws ModifyException{
+	void updateFavorites(String memberId, List<LectureCategory> favorite_list)throws ModifyException{
 		
 	}
+
 	
 	// 회원 목록 보기(admin) : 영민
 }
--- ShallWe/src/main/java/com/shallwe/service/MemberSerivce.java
+++ ShallWe/src/main/java/com/shallwe/service/MemberSerivce.java
@@ -1,8 +1,32 @@
 package com.shallwe.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import com.shallwe.dao.MemberDAO;
+import com.shallwe.exception.FindException;
+import com.shallwe.exception.ModifyException;
+import com.shallwe.model.MemberInfoBean;
 
 @Service(value = "memberService")
 public class MemberSerivce {
 
+	private MemberDAO dao;
+	
+	@Autowired
+	@Qualifier(value="memberDAO")
+	public MemberDAO getDao() {
+		return dao;
+	}
+
+	public void setDao(MemberDAO dao) {
+		this.dao = dao;
+	}
+	public MemberInfoBean findById(String memberId)throws FindException{ //상하 : 내 정보 보기 
+		return dao.selectById(memberId);
+	}
+	public void updatePwd(String memberId, String memberPassword)throws ModifyException {
+		dao.updatePassword(memberId, memberPassword);
+	}
 }
