c465fcbae51fcaee249886ab5e06bdda6bc96053
kimsangha617 <66237694+kimsangha617@users.noreply.github.com>
2020-09-18 11:33:36 +0900

    회원가입test

--- ShallWe/src/main/java/com/shallwe/dao/MemberDAO.java
+++ ShallWe/src/main/java/com/shallwe/dao/MemberDAO.java
@@ -30,12 +30,15 @@ public class MemberDAO {
 	
 
 	// 회원가입 : 상하
-//	public void joinMember(Map<String, Object>joinmem, MemberInfoBean mib) throws AddException{
-//		SqlSession session=null;
-//		session = sqlSessionFactory.openSession();
-//		mib = session.insert("MemberMapper.joinMember", parameter)
-		
-//	}
+	public void joinMember(MemberInfoBean mib) throws AddException{
+		SqlSession session=null;
+		try {
+			session = sqlSessionFactory.openSession();
+			session.insert("MemberMapper.joinMember", mib);
+		}catch(Exception e){
+			throw new AddException(e.getMessage());
+		}
+	}
 	// 내 정보 보기(수강생) : 상하
 	public MemberInfoBean selectById(String memberId) throws FindException
 	{
