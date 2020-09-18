3b678bcf0dd6ce4cb5770f6e50e9c833eb339f83
SOOJEONG-DESKTO\Soojeong <kosj1109@kakao.com>
2020-09-18 10:31:56 +0900

    생성자 없는 bean 추가, 모든 파라미터 가지고 있는 생성자 추가, 테스트 파일 test 패키지 안으로 이동

--- ShallWe/src/main/java/com/shallwe/model/LectureSearchBean.java
+++ ShallWe/src/main/java/com/shallwe/model/LectureSearchBean.java
@@ -5,13 +5,17 @@ import java.util.Date;
 import com.shallwe.vo.LectureCategory;
 import com.shallwe.vo.Tutor;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Soojeong
  * 메인 - 검색어 입력 시 조회결과화면 출력
  * 검색 결과 화면 내에서 재검색 
  */
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class LectureSearchBean {
 	private int lecture_id;
