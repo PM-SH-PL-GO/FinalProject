260767b6f3edb1dd3dcb2cd8aceaa6f8458245a9
SOOJEONG-DESKTO\Soojeong <kosj1109@kakao.com>
2020-09-19 20:34:07 +0900

    강의검색결과, 강의검색기능 (mapper, DAO, service, controller ) test완료.,     LectureSearchBean 삭제(사용안함)

--- ShallWe/src/main/java/com/shallwe/control/kosjController.java
+++ ShallWe/src/main/java/com/shallwe/control/kosjController.java
@@ -1,15 +1,26 @@
 package com.shallwe.control;
 
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.shallwe.exception.FindException;
+import com.shallwe.service.LectureService;
 
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 @Controller
 public class kosjController {
-
+	
+	@Autowired
+	LectureService lectureService;
+	
 	@RequestMapping(value = "/lecturePaid", method = RequestMethod.GET)
 	public void lecturePaid() {
 		System.out.println("lecturePaid.jsp 호출");
@@ -21,6 +32,42 @@ public class kosjController {
 	@RequestMapping(value = "/searchResult", method = RequestMethod.GET)
 	public void searchResult() {
 		System.out.println("searchResult.jsp  호출");
+	}
+	
+	@RequestMapping(value = "/search", method = RequestMethod.POST)
+	public ModelAndView search(@RequestParam(value="searchKey")String searchKeyParam
+							 , @RequestParam(value="searchText")String searchText) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		
+		// 입력 받은 검색어 param null값 처리
+		if ( ("").equals(searchText) ) {
+			searchText = " ";
+		} 
+		
+		// 검색조건 요청 잘못 들어온 경우 처리
+		int searchKey = Integer.parseInt(searchKeyParam);
+		if ( searchKey > 3 ) {
+			searchKey = 0;
+		}
+		
+		String [] searchKeyArr = {"all", "tutor_name", "lecture_title" , "category" };
+		map.put("searchKey", searchKeyArr[searchKey]);
+		map.put("searchText", searchText);
+		
+		ModelAndView modelAndView = new ModelAndView();
+
+		try {
+			modelAndView = lectureService.searchLecture(map);
+			log.info(modelAndView.getModelMap()); 
+			modelAndView.setViewName("/searchLectureList");
+			
+			
+		} catch (FindException e) {
+			modelAndView.setViewName("/fail");
+			e.printStackTrace();
+		}
+		
+		return modelAndView;
 	}
 //	@RequestMapping(value = "/top", method = RequestMethod.GET)
 //	public void top() {
