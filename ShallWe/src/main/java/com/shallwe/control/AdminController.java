58cc896f5e5773afbd3067a83d2cfb1f4be0e281
captic94 <seilu94@naver.com>
2020-09-21 16:59:52 +0900

    admin 특정 강사 목록 가져오기

--- ShallWe/src/main/java/com/shallwe/control/AdminController.java
+++ ShallWe/src/main/java/com/shallwe/control/AdminController.java
@@ -3,6 +3,7 @@ package com.shallwe.control;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,16 +12,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shallwe.exception.FindException;
 import com.shallwe.exception.ModifyException;
 //import com.shallwe.model.FaqBean;
 import com.shallwe.service.AdminService;
 import com.shallwe.vo.Faq;
+import com.shallwe.vo.Lecture;
 import com.shallwe.vo.Tutor;
 
 @Controller
@@ -34,6 +40,12 @@ public class AdminController {
 		System.out.println("Admin Index");
 	}
 	
+	
+	/**
+	 * 예비강사 목록 가져오기
+	 * @author jun6
+	 * @return
+	 */
 	@RequestMapping(value = "/preTutor")
 	public ModelAndView preTutor() {
 		ModelAndView mnv = new ModelAndView();
@@ -48,6 +60,11 @@ public class AdminController {
 		return mnv;
 	}
 	
+	/**
+	 * 전체 강사 목록 가져오기
+	 * @author jun6
+	 * @return
+	 */
 	@RequestMapping(value = "/tutorList")
 	public ModelAndView tutorList() {
 		ModelAndView mnv = new ModelAndView();
@@ -69,17 +86,43 @@ public class AdminController {
 	 * @param status
 	 * @return
 	 */
-	@PatchMapping(value = "/status/{tutorId}")
-	public ResponseEntity<String> statusChange(@PathVariable(value = "tutorId")String id, @RequestParam(name="status") String status){
-		System.out.println("/////////////////////////////////"+ id + " : " + status + "/////////////////////////////////");
+	@PatchMapping(value = "/status/{tutorId}"
+			//,consumes = {"application/json; charset=UTF-8"}
+			)
+	public ResponseEntity<String> statusChange(@PathVariable(value = "tutorId")String id, @RequestBody String status){
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, Object> map;
+		String tutorStatus = "";
+		try {
+			map = mapper.readValue(status, new TypeReference<Map<String, Object>>() {});
+			tutorStatus = map.get("status").toString();
+		} catch (JsonMappingException e1) {
+			e1.printStackTrace();
+		} catch (JsonProcessingException e1) {
+			e1.printStackTrace();
+		}
 		try{
-			adminService.approvePreTutor(id, status);
+			adminService.approvePreTutor(id, tutorStatus);
 			return (ResponseEntity<String>) ResponseEntity.status(HttpStatus.OK).body("정상적으로 설정되었습니다");
 		}catch(ModifyException e) {
 			e.printStackTrace();
 			return (ResponseEntity<String>) ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("설정에 실패하였습니다");
 		}
 	}
+	
+	@RequestMapping(value = "/tutorLecture/{tutorId}")
+	public ResponseEntity<List<Lecture>> tutorLectureList(@PathVariable(value = "tutorId") String tutor_id){
+		List<Lecture> lectureList = new ArrayList<>();
+		try {
+			lectureList = adminService.ShowTutorLectureByTutorId(tutor_id);
+		}catch(FindException e) {
+			e.printStackTrace();
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(lectureList);
+	}
+	
+	
 	
 	@RequestMapping(value = "/userList", method = RequestMethod.GET)
 	public void userList(Locale locale, Model model) {
@@ -95,6 +138,11 @@ public class AdminController {
 	public void memberLectureHistory(Locale locale, Model model) {
 		System.out.println("memberLectureHistory coming");
 	}
+	
+	
+	
+	
+	/////////////////////////FAQ////////////////////////////
 	
 	/**
 	 * FAQ 조회
@@ -115,4 +163,6 @@ public class AdminController {
 		
 		return mnv;
 	}
+	
+	
 }
