29b8db78281d3233eac744197063bdd07df8362f
SDI998 <tta1424@naver.com>
2020-10-07 11:41:23 +0900

    수정 중

--- ShallWe/src/main/java/com/shallwe/control/LectureController.java
+++ ShallWe/src/main/java/com/shallwe/control/LectureController.java
@@ -311,6 +311,7 @@ public class LectureController {
 		try {
 			tutorlist = tutoser.showTutorInfo(id);
 			mnv.addObject("tutorlist", tutorlist);
+			System.out.println("강사!"+tutorlist);
 			mnv.setViewName("/lectureDetail");
 		} catch (FindException e1) {
 			e1.printStackTrace();
